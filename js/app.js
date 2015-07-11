daccb709b13855144231cdc7d3b437ec003c2881
Andrea Mckenzie <ma3east@gmail.com>
2015-07-11 19:12:51 +0100

    commiting pseudo code on app.js file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+/*this game v1 is a group game, a player inputs the word they wish to use for the game (this is hidden from other users), and the group have to guess the word using letters from the alphabet before the hangman is fully constructed.  if time may implement v2 which also includes a single player mode*/
+
+//load the initial page displaying a welcome to the game
+//have access to a section/or via button click, which explains how the game works
+//load the area to contain the hangman
+//load the input box to collect the letters to be played in the game
+//request input from the user to enter the letters to use in the game
+//do validation to check for letters, no numbers
+//store the letters in a container file
+//ensure the letters are not displayed on screen
+//ensure the game is won, if the guessed letters are matched before the hangman is fully constructed
+//ensure the game is lost, when the letters collected have not been matched to the required word, before the hangman has been fully constructed.
+//On game over if incorrect need to display the word that was input.
+//Need to keep scores, and allow scores to be cleared.
+
+
+
+
 $(document).ready(function() {
 
 
